50c2d82b0068d7ea5dc733c14ae047d329f3c592
Anton Webb <anton25360@gmail.com>
2020-04-08 14:43:15 +0100

    Changed api

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import unicornhat as uh
 uh.set_layout(uh.PHAT) #uses small layout
-uh.brightness(0.5)
+uh.brightness(0.3)
 
 uh.set_pixel(0, 0, 255, 0, 0)
+uh.set_pixel(0, 1, 0, 255, 0)
+uh.set_pixel(2, 0, 0, 0, 255)
 uh.show()
+
+import requests
+
+
+# url = "https://covidapi.info/api/v1/global"
+
+response = requests.get('https://covidapi.info/api/v1/global')
+print(response)
+
+
+
+
+
 
 print("running main.py")
 input() #makes script consistent
