b4b2a7747dca8583de911b45c92acc5a566934f1
Anton Webb <anton25360@gmail.com>
2020-04-08 12:50:58 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import unicornhat as uh
+uh.set_layout(uh.PHAT) #uses small layout
+uh.brightness(0.5)
+
+uh.set_pixel(0, 0, 255, 0, 0)
+uh.show()
+
+# print("hello dude")
+input() #makes script consistent
