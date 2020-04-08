38374a155050a79e4c7de00fa2eac3515ea1bb6a
Anton Webb <anton25360@gmail.com>
2020-04-08 14:45:29 +0100

    renamed requests to r

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ uh.set_pixel(0, 1, 0, 255, 0)
 uh.set_pixel(2, 0, 0, 0, 255)
 uh.show()
 
-import requests
+import requests as r
 
 
 # url = "https://covidapi.info/api/v1/global"
 
-response = requests.get('https://covidapi.info/api/v1/global')
-print(response)
+response = r.get('https://covidapi.info/api/v1/global')
+print(response.headers)
 
 
 
